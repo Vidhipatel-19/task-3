748fcaa6781e9a483b9e0dae61de48ae3d205004
vidhipatel-19 <vidhipatel66501@gamil.com>
2025-03-12 10:54:23 -0400

    post-status

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Table from './components/Table'
 import Form from './components/Form'
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
               <Route path='/' element={<Table/>}/>
               <Route path='/form' element={<Form/>}/>
            </Routes>
+           <ToastContainer
+              position="bottom-right"
+              
+           />
         </BrowserRouter>
     </div>
   )
