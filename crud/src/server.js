42687efb87de477b687751da975982549ebd69ff
vidhipatel-19 <vidhipatel66501@gamil.com>
2025-03-12 12:00:39 -0400

    post-data

--- crud/src/server.js
+++ crud/src/server.js
@@ -3,14 +3,20 @@ import  axios from 'axios'
 const API = "http://localhost:3002/users"
 
 async function getData(){
-  const response =  await axios.get(API)
-  return response.data
+  try{
+    const response =  await axios.get(API)
+    return response.data
+  }
+  catch(error){
+    console.log(error)
+  }
 }
 
 async function postData(obj){
   try {
     await axios.post(API, obj)
   } catch (error) {
+
     console.log(error)
   }
 }
