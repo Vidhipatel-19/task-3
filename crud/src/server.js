f15084e20b04ccb02e12065303846e2932989c00
vidhipatel-19 <vidhipatel66501@gamil.com>
2025-03-17 09:51:02 -0500

    delete data

--- crud/src/server.js
+++ crud/src/server.js
@@ -3,23 +3,25 @@ import  axios from 'axios'
 const API = "http://localhost:3002/users"
 
 async function getData(){
-  try{
-    const response =  await axios.get(API)
-    return response.data
-  }
-  catch(error){
-    console.log(error)
-  }
+  const response =  await axios.get(API)
+  return response.data
 }
 
 async function postData(obj){
   try {
     await axios.post(API, obj)
   } catch (error) {
+    console.log(error)
+  }
+}
 
+async function deleteData(id){
+  try {
+    await axios.delete(`http://localhost:3002/users/${id}`)
+  } catch (error) {
     console.log(error)
   }
 }
 
 
-export {getData, postData}
+export {getData, postData, deleteData}
